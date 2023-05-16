bc7f3b9721dbbb485ba39e30886d25107da5e2c5
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2023-05-16 20:40:59 +0200

    switching to netlify

--- source/conf.py
+++ source/conf.py
@@ -91,6 +91,6 @@ html_show_copyright = False
 
 # Intersphinx
 intersphinx_mapping = {
-    "blue": ("https://blue.tymyrddin.dev/en/latest/", None),
+    "blue": ("https://blue.tymyrddin.dev/", None),
 }
 myst_url_schemes = ["http", "https", ]
